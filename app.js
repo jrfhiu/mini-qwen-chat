f37518146acc3719afce0e7367b3940da1d5634d
jrfhiu <2644801322@qq.com>
2026-04-10 09:12:23 +0800

    Update app.js

--- app.js
+++ app.js
@@ -6,32 +6,32 @@ function send() {
     if (!msg) return;
 
     let chat = document.getElementById("chat-box");
-    
     chat.innerHTML += `<div class="user">${msg}</div>`;
     input.value = "";
     chat.scrollTop = chat.scrollHeight;
 
+    // 最简单、最稳定、永远不会报错的调用方式
     fetch(API_URL, {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify({
-            data: [msg, []]
+            data: [msg]
         })
     })
     .then(res => res.json())
     .then(data => {
         try {
-            let reply = data.data[0][1][1];
+            let reply = data.data[0];
             chat.innerHTML += `<div class="bot">${reply}</div>`;
-        } catch(e) {
-            chat.innerHTML += `<div class="bot">我是 mini_qwen_1b 模型！</div>`;
+        } catch (e) {
+            chat.innerHTML += `<div class="bot">我是 mini_qwen_1b AI 模型！</div>`;
         }
         chat.scrollTop = chat.scrollHeight;
     })
-    .catch(e => {
-        chat.innerHTML += `<div class="bot">模型正在启动，请稍候重试...</div>`;
+    .catch(() => {
+        chat.innerHTML += `<div class="bot">模型正在启动，请稍等 10 秒再发一次！</div>`;
         chat.scrollTop = chat.scrollHeight;
     });
 }
